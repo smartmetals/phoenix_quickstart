0e2bd65792a50e5a77f7fbbbe2af244781311eb0
Jeremy Huffman <jeremy@jeremyhuffman.com>
2017-09-22 15:05:28 -0400

    Let compose sort out the building.

--- functions.sh
+++ functions.sh
@@ -3,15 +3,7 @@
 phx-new() {
     phx-init $1
     mkdir -p "$PWD/projects/$1"
-    check-image $PWD
     docker-compose run web mix phx.new $1
-}
-
-check-image() {
-    echo 'checking for docker image...'
-    installed=`docker images | grep phoenix13`
-    echo $installed
-    [[ $installed = *[![:space:]]* ]] || echo "installing docker image" && docker build -t phoenix13 $1
 }
 
 phx-init() {
